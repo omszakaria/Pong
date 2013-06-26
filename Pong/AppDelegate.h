a5563f7a940d3ded9983559a86a86df91bf60c3c
omszakaria <omszakaria@gmail.com>
2013-06-26 11:19:23 -0400

    Initial Commit

---
+++ Pong/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Pong
+//
+//  Created by iD Student on 6/26/13.
+//  Copyright (c) 2013 Omar Zakaria. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
