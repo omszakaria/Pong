b725eaf61fa4736faf19c5b796bc1f2a36fb266d
omszakaria <omszakaria@gmail.com>
2013-06-26 12:42:39 -0400

    Lunch 6/26/13

---
+++ OptionsViewController.h
@@ -0,0 +1,13 @@
+//
+//  OptionsViewController.h
+//  Pong
+//
+//  Created by iD Student on 6/26/13.
+//  Copyright (c) 2013 Omar Zakaria. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OptionsViewController : UIViewController
+
+@end
---
+++ OptionsViewController.m
@@ -0,0 +1,38 @@
+//
+//  OptionsViewController.m
+//  Pong
+//
+//  Created by iD Student on 6/26/13.
+//  Copyright (c) 2013 Omar Zakaria. All rights reserved.
+//
+
+#import "OptionsViewController.h"
+
+@interface OptionsViewController ()
+
+@end
+
+@implementation OptionsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
